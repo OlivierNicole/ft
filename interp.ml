c77a919e278f52ed6f65ea702af30b2bcd65ef48
Olivier Nicole <nicole@ensta.fr>
2017-01-06 17:31:56 +0000

    Add first-class types and make syntax nicer.

--- interp.ml
+++ interp.ml
@@ -1,10 +1,24 @@
 type 'a expression = 'a
 
 (*
+ * Underlying OCaml types
+ *)
+type ipv4_address = int * int * int * int
+
+(*
  * Types.
  *)
-type integer = int
-type ipv4_address = integer * integer * integer * integer
+type _ typ =
+  | Product : 'a typ * 'b typ -> ('a * 'b) typ
+  | IPv4Address : (int * int * int * int) typ
+  | Int : int typ
+  | String : string typ
+  | Bool : bool typ
+let ( ** ) x y = Product (x, y)
+let int = Int
+let string = String
+let bool = Bool
+let ipv4_address = IPv4Address
 
 (*
  * Channels.
@@ -15,47 +29,47 @@ type channel_inverted = bool
 (*
  * Function definitions.
  *)
-type ('dom, 'ret) fn = 'dom -> 'ret expression
+type ('dom, 'ret) fn = 'dom expression -> 'ret expression
 
-let decl_fn f = f
+let fn _ _ f = f
 
 (*
  * Expressions.
  *)
 let true_ = true
 let false_ = false
-let and_ = (&&)
-let or_ = (||)
-let not_ b = not b
-let equals = (=)
-let greater_than = (>=)
-let less_than = (<=)
-let int i = i
-let plus = (+)
-let minus = (-)
-let times = ( * )
+let (&&) = (&&)
+let (||) = (||)
+let not b = not b
+let (=) = (=)
+let (>=) = (>=)
+let (<=) = (<=)
+let mkint i = i
+let (+) = (+)
+let (-) = (-)
+let ( * ) = ( * )
 let mod_ a b = a mod b
-let quotient = (/)
+let (/) = (/)
 let abs = Pervasives.abs
-let ipv4_address addr = addr
+let mk_ipv4_address addr = addr
 let int_to_address i =
   (i land 0xff000000, i land 0xff0000, i land 0xff00, i land 0xff)
 let address_to_int (a, b, c, d) =
   (a lsl 24) lor (b lsl 16) lor (c lsl 8) lor d
-let empty_list = []
-let cons_list x xs = x :: xs
-let append_list = (@)
+let empty = []
+let (^::) x xs = x :: xs
+let (@) = (@)
 let pair x y = x, y
-let update r x = r := x
-let first x = fst x
-let second x = snd x
-let fn_apply f x = f x
+let (:=) r x = r := x
+let fst x = fst x
+let snd x = snd x
+let apply f x = f x
 let rec integer_range lb ub =
   if ub <= lb then []
   else
     let new_ub = pred ub in
     new_ub :: integer_range lb new_ub
-let send _inverted _chan _x = ()
-let receive _inverted chan = chan
+let (!) _inverted _chan _x = ()
+let (?.) _inverted chan = chan
 let peek _inverted _chan = ()
-let str s = s
+let mkstr s = s
--- language.mli
+++ language.mli
@@ -1,14 +1,21 @@
 type 'a expression
 
 (*
- * Types
+ * Underlying OCaml types
+ *)
+type ipv4_address
+
+(*
+ * Flick types
  *)
 (* Analogous of the "type_value" variant. *)
-(* string: use OCaml's *)
-type integer
-(* boolean: use OCaml's *)
+type _ typ
+val ( ** ) : 'a typ -> 'b typ -> ('a * 'b) typ
+val string : string typ
+val int : int typ
+val bool : bool typ
 (* record_type: not supported yet *)
-type ipv4_address = integer * integer * integer * integer
+val ipv4_address : ipv4_address typ
 
 (*
  * Channels
@@ -20,36 +27,37 @@ type channel_inverted = bool
 (*
  * Function definitions
  *)
-(* Since currying is not available (functions are not first-class), we will
- * encode multiple arguments as tuple types. *)
+(* Note: since currying is not available (functions are not first-class), multiple
+ * arguments should be encoded as tuples. *)
 type ('dom, 'ret) fn
-val decl_fn : ('dom -> 'ret expression) -> ('dom, 'ret) fn
+val fn : 'dom typ -> 'ret typ ->
+  ('dom expression -> 'ret expression) -> ('dom, 'ret) fn
 
 (*
  * Expressions
  *)
 val true_ : bool expression
 val false_ : bool expression
-val and_ : bool expression -> bool expression -> bool expression
-val or_ : bool expression -> bool expression -> bool expression
-val not_ : bool expression -> bool expression
-val equals : 'a expression -> 'a expression -> bool expression
-val greater_than : 'a expression -> 'a expression -> bool expression
-val less_than : 'a expression -> 'a expression -> bool expression
-val int : integer -> integer expression
-val plus : integer expression -> integer expression -> integer expression
-val minus : integer expression -> integer expression -> integer expression
-val times : integer expression -> integer expression -> integer expression
-val mod_ : integer expression -> integer expression -> integer expression
-val quotient : integer expression -> integer expression -> integer expression
-val abs : integer expression -> integer expression
-val ipv4_address : (integer * integer * integer * integer) expression
+val (&&) : bool expression -> bool expression -> bool expression
+val (||) : bool expression -> bool expression -> bool expression
+val not : bool expression -> bool expression
+val (=) : 'a expression -> 'a expression -> bool expression
+val (>=) : 'a expression -> 'a expression -> bool expression
+val (<=) : 'a expression -> 'a expression -> bool expression
+val mkint : int -> int expression
+val (+) : int expression -> int expression -> int expression
+val (-) : int expression -> int expression -> int expression
+val ( * ) : int expression -> int expression -> int expression
+val mod_ : int expression -> int expression -> int expression
+val (/) : int expression -> int expression -> int expression
+val abs : int expression -> int expression
+val mk_ipv4_address : (int * int * int * int) expression
   -> ipv4_address expression
-val int_to_address : integer expression -> ipv4_address expression
-val address_to_int : ipv4_address expression -> integer expression
-val empty_list : 'a list expression
-val cons_list : 'a expression -> 'a list expression -> 'a list expression
-val append_list : 'a list expression -> 'a list expression
+val int_to_address : int expression -> ipv4_address expression
+val address_to_int : ipv4_address expression -> int expression
+val empty : 'a list expression
+val (^::) : 'a expression -> 'a list expression -> 'a list expression
+val (@) : 'a list expression -> 'a list expression
   -> 'a list expression
 (* TupleValue will have to be simulated using nested pairs. *)
 val pair : 'a expression -> 'b expression -> ('a * 'b) expression
@@ -59,22 +67,22 @@ val pair : 'a expression -> 'b expression -> ('a * 'b) expression
 (*val ite : bool expression -> 'a expression -> 'a expression option*)
 (*val par : ?*)
 (* if-then-else *)
-val update : 'a ref expression -> 'a expression -> unit expression
+val (:=) : 'a ref expression -> 'a expression -> unit expression
 (*val update_indexable : ?*)
 (* RecordProjection: records not supported yet, using this instead: *)
-val first : ('a * _) expression -> 'a expression
-val second : (_ * 'b) expression -> 'b expression
+val fst : ('a * _) expression -> 'a expression
+val snd : (_ * 'b) expression -> 'b expression
 (* Don't know why in motto code the constructor is "Functor_App"; here I will
  * simply name it "fn_apply". *)
-val fn_apply : ('dom, 'ret) fn -> 'dom expression -> 'ret expression
-val integer_range : integer expression -> integer expression
-  -> integer list expression
+val apply : ('dom, 'ret) fn -> 'dom expression -> 'ret expression
+val integer_range : int expression -> int expression
+  -> int list expression
 (*val map : ?*)
 (*val iterate : ?*)
-val send : channel_inverted -> (_, 'a) channel -> 'a expression -> unit expression
-val receive : channel_inverted -> ('a, _) channel -> 'a expression
+val (!) : channel_inverted -> (_, 'a) channel -> 'a expression -> unit expression
+val (?.) : channel_inverted -> ('a, _) channel -> 'a expression
 val peek : channel_inverted -> _ channel -> unit expression
-val str : string -> string expression
+val mkstr : string -> string expression
 (*val meta_quoted : ?*)
 (*val hole : ?*)
 (*val literal_expr : ?*)
