42c7c671a3f2efe5f456e2255601c6efebbdf371
Olivier Nicole <nicole@ensta.fr>
2017-02-15 11:50:44 +0000

    Abstract away the interpreter

--- language.mli
+++ language.mli
@@ -1,118 +1,121 @@
-type 'a expression
+module type Semantics = sig
 
-(*
- * Underlying OCaml types
- *)
-type ipv4_address
+  type 'a expression
 
-(*
- * Flick types
- *)
-(* Analogous of the "type_value" variant. *)
-type _ typ
-val ( ** ) : 'a typ -> 'b typ -> ('a * 'b) typ
-val string : string typ
-val int : int typ
-val bool : bool typ
-val void : unit typ
-(* record_type: not supported yet *)
-val ipv4_address : ipv4_address typ
+  (*
+   * Underlying OCaml types
+   *)
+  type ipv4_address
 
-(*
- * Channels
- *)
-(* Channel receives values of type 'o and sends values of type 'i *)
-type ('i, 'o) channel
+  (*
+   * Flick types
+   *)
+  (* Analogous of the "type_value" variant. *)
+  type _ typ
+  val ( ** ) : 'a typ -> 'b typ -> ('a * 'b) typ
+  val string : string typ
+  val int : int typ
+  val bool : bool typ
+  val void : unit typ
+  (* record_type: not supported yet *)
+  val ipv4_address : ipv4_address typ
 
-type channel_inverted = bool
+  (*
+   * Channels
+   *)
+  (* Channel receives values of type 'o and sends values of type 'i *)
+  type ('i, 'o) channel
 
-type void (* Empty type *)
+  type channel_inverted = bool
 
-type (_, _) chan_type =
-  | Io : 'i typ * 'o typ -> ('i, 'o) chan_type
+  type void (* Empty type *)
 
-val channel : ('i, 'o) chan_type -> ('i, 'o) channel
-(* Can read from channel? *)
-val can : (_, _) channel -> bool expression
-val put_incoming : ('a, _) channel -> 'a expression -> unit
-val put_outgoing : (_, 'a) channel -> 'a expression -> unit
+  type (_, _) chan_type =
+    | Io : 'i typ * 'o typ -> ('i, 'o) chan_type
 
-(*
- * Function definitions
- *)
-(* Note: since currying is not available (functions are not first-class),
- * multiple arguments should be encoded as tuples. *)
-type ('dom, 'ret) fn
-val fn : 'dom typ -> 'ret typ ->
-  ('dom expression -> 'ret expression) -> ('dom, 'ret) fn
+  val channel : ('i, 'o) chan_type -> ('i, 'o) channel
+  (* Can read from channel? *)
+  val can : (_, _) channel -> bool expression
+  val put_incoming : ('a, _) channel -> 'a expression -> unit
+  val put_outgoing : (_, 'a) channel -> 'a expression -> unit
 
-(*
- * Processes
- *)
-(*
-type _ chan_types =
-  | Nil : unit chan_types
-  | Cons : 'a * 'b chan_types -> ('a * 'b) chan_types
+  (*
+   * Function definitions
+   *)
+  (* Note: since currying is not available (functions are not first-class),
+   * multiple arguments should be encoded as tuples. *)
+  type ('dom, 'ret) fn
+  val fn : 'dom typ -> 'ret typ ->
+    ('dom expression -> 'ret expression) -> ('dom, 'ret) fn
 
-type 'chans process
-*)
+  (*
+   * Processes
+   *)
+  (*
+  type _ chan_types =
+    | Nil : unit chan_types
+    | Cons : 'a * 'b chan_types -> ('a * 'b) chan_types
 
-(*
- * Expressions
- *)
-val true_ : bool expression
-val false_ : bool expression
-val (&&) : bool expression -> bool expression -> bool expression
-val (||) : bool expression -> bool expression -> bool expression
-val not : bool expression -> bool expression
-val (=) : 'a expression -> 'a expression -> bool expression
-val (>=) : 'a expression -> 'a expression -> bool expression
-val (<=) : 'a expression -> 'a expression -> bool expression
-val mkint : int -> int expression
-val (+) : int expression -> int expression -> int expression
-val (-) : int expression -> int expression -> int expression
-val ( * ) : int expression -> int expression -> int expression
-val mod_ : int expression -> int expression -> int expression
-val (/) : int expression -> int expression -> int expression
-val abs : int expression -> int expression
-val mk_ipv4_address : (int * int * int * int) expression
-  -> ipv4_address expression
-val int_to_address : int expression -> ipv4_address expression
-val address_to_int : ipv4_address expression -> int expression
-val empty : 'a list expression
-val (^::) : 'a expression -> 'a list expression -> 'a list expression
-val (@) : 'a list expression -> 'a list expression
-  -> 'a list expression
-(* TupleValue will have to be simulated using nested pairs. *)
-val pair : 'a expression -> 'b expression -> ('a * 'b) expression
-(* Seq and if-then-else: cannot be implemented as a function because of strict
- * evaluation. *)
-val seq : unit expression -> 'b expression -> 'b expression
-val if_ : bool expression -> 'a expression -> 'a expression -> 'a expression
-(*val par : ?*)
-(* if-then-else *)
-val (:=) : 'a ref expression -> 'a expression -> unit expression
-(*val update_indexable : ?*)
-(* RecordProjection: records not supported yet, using this instead: *)
-val fst : ('a * _) expression -> 'a expression
-val snd : (_ * 'b) expression -> 'b expression
-(* Don't know why in motto code the constructor is "Functor_App"; here I will
- * simply name it "fn_apply". *)
-val apply : ('dom, 'ret) fn -> 'dom expression -> 'ret expression
-val integer_range : int expression -> int expression
-  -> int list expression
-(*val map : ?*)
-val iterate :
-  'a list expression (* List *)
-  -> 'b expression (* Initial value *)
-  -> ('b expression -> 'a expression -> 'b expression) (* Loop body *)
-  -> 'b expression
-val (<~) : (_, 'a) channel -> 'a expression -> unit expression
-val (?.) : ('a, _) channel -> 'a expression
-val (??) : ('a, _) channel -> 'a expression (* Peeking *)
-val mkstr : string -> string expression
-(*val meta_quoted : ?*)
-(*val hole : ?*)
-(*val literal_expr : ?*)
+  type 'chans process
+  *)
 
-val eval : 'a expression -> 'a
+  (*
+   * Expressions
+   *)
+  val true_ : bool expression
+  val false_ : bool expression
+  val (&&) : bool expression -> bool expression -> bool expression
+  val (||) : bool expression -> bool expression -> bool expression
+  val not : bool expression -> bool expression
+  val (=) : 'a expression -> 'a expression -> bool expression
+  val (>=) : 'a expression -> 'a expression -> bool expression
+  val (<=) : 'a expression -> 'a expression -> bool expression
+  val mkint : int -> int expression
+  val (+) : int expression -> int expression -> int expression
+  val (-) : int expression -> int expression -> int expression
+  val ( * ) : int expression -> int expression -> int expression
+  val mod_ : int expression -> int expression -> int expression
+  val (/) : int expression -> int expression -> int expression
+  val abs : int expression -> int expression
+  val mk_ipv4_address : (int * int * int * int) expression
+    -> ipv4_address expression
+  val int_to_address : int expression -> ipv4_address expression
+  val address_to_int : ipv4_address expression -> int expression
+  val empty : 'a list expression
+  val (^::) : 'a expression -> 'a list expression -> 'a list expression
+  val (@) : 'a list expression -> 'a list expression
+    -> 'a list expression
+  (* TupleValue will have to be simulated using nested pairs. *)
+  val pair : 'a expression -> 'b expression -> ('a * 'b) expression
+  (* Seq and if-then-else: cannot be implemented as a function because of strict
+   * evaluation. *)
+  val seq : unit expression -> 'b expression -> 'b expression
+  val if_ : bool expression -> 'a expression -> 'a expression -> 'a expression
+  (*val par : ?*)
+  (* if-then-else *)
+  val (:=) : 'a ref expression -> 'a expression -> unit expression
+  (*val update_indexable : ?*)
+  (* RecordProjection: records not supported yet, using this instead: *)
+  val fst : ('a * _) expression -> 'a expression
+  val snd : (_ * 'b) expression -> 'b expression
+  (* Don't know why in motto code the constructor is "Functor_App"; here I will
+   * simply name it "fn_apply". *)
+  val apply : ('dom, 'ret) fn -> 'dom expression -> 'ret expression
+  val integer_range : int expression -> int expression
+    -> int list expression
+  (*val map : ?*)
+  val iterate :
+    'a list expression (* List *)
+    -> 'b expression (* Initial value *)
+    -> ('b expression -> 'a expression -> 'b expression) (* Loop body *)
+    -> 'b expression
+  val (<~) : (_, 'a) channel -> 'a expression -> unit expression
+  val (?.) : ('a, _) channel -> 'a expression
+  val (??) : ('a, _) channel -> 'a expression (* Peeking *)
+  val mkstr : string -> string expression
+  (*val meta_quoted : ?*)
+  (*val hole : ?*)
+  (*val literal_expr : ?*)
+
+  val eval : 'a expression -> 'a
+end
--- test.ml
+++ test.ml
@@ -1,17 +1,23 @@
-open Interp
+module Test = functor (Backend : Language.Semantics) -> struct
 
-let program =
-  let y = mkint 13 in
-  let somechan = channel (Io (int, void)) in
-  put_incoming somechan (mkint 4);
-  put_incoming somechan (mkint 2);
-  let factorial =
-    fn int int (fun n ->
-      iterate (integer_range (mkint 1) (n+mkint 1)) (mkint 1)
-        (fun acc x ->
-          x * acc))
-  in
-  let z = mkint 5 + (y * ?.somechan) + y in
-  apply factorial (z - (mkint 60 + ?.somechan))
+  open Backend
 
-let () = Printf.printf "%d\n" @@ eval program
+  let program =
+    let y = mkint 13 in
+    let somechan = channel (Io (int, void)) in
+    put_incoming somechan (mkint 4);
+    put_incoming somechan (mkint 2);
+    let factorial =
+      fn int int (fun n ->
+        iterate (integer_range (mkint 1) (n+mkint 1)) (mkint 1)
+          (fun acc x ->
+            x * acc))
+    in
+    let z = mkint 5 + (y * ?.somechan) + y in
+    apply factorial (z - (mkint 60 + ?.somechan))
+
+end
+
+module Program = Test(Interp)
+
+let () = Printf.printf "%d\n" @@ Interp.eval Program.program
